ea9389d1e2cb6bfc555647058a5ddab53e728655
vraparthi <vraparthi@proofpoint.com>
2020-06-27 15:12:56 -0700

    Removed template

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,7 +5,7 @@
 # Copyright:: 2020, The Authors, All Rights Reserved.
 
 yum_package 'java-1.7.0-openjdk' do
-  action :upgrade
+  action [ :install, :upgrade ]
 end
 
 group node['tomcat']['group'] do
@@ -33,17 +33,9 @@ archive_file node['tomcat']['download_location'] do
   group node['tomcat']['group']
   mode '0754'
   owner node['tomcat']['user']
+  options [ "--strip-components=1" ]
   action :extract
 end
-
-#template node['tomcat']['systemd_file_path'] do
-#  backup false
-#  group node['tomcat']['group']
-#  owner node['tomcat']['user']
-#  mode '0744'
-#  source 'centos/tomcat.service.erb'
-#  action :create_if_missing
-#end
 
 systemd_unit 'tomcat.service' do
   content <<-EOU.gsub(/^\s+/, '')
@@ -54,17 +46,17 @@ systemd_unit 'tomcat.service' do
   [Service]
   Type=forking
 
-  Environment=JAVA_HOME=node['java']['java_home']
-  Environment=CATALINA_PID=node['tomcat']['catalina_pid']
-  Environment=CATALINA_HOME=node['tomcat']['home_dir']
-  Environment=CATALINA_BASE=node['tomcat']['home_dir']
-  Environment="CATALINA_OPTS=node['tomcat']['catalina_opts']""
+  Environment=JAVA_HOME=#{node['java']['java_home']}
+  Environment=CATALINA_PID=#{node['tomcat']['catalina_pid']}
+  Environment=CATALINA_HOME=#{node['tomcat']['home_dir']}
+  Environment=CATALINA_BASE=#{node['tomcat']['home_dir']}
+  Environment="CATALINA_OPTS=#{node['tomcat']['catalina_opts']}"
 
-  ExecStart=node['tomcat']['startup_script']
+  ExecStart=#{node['tomcat']['startup_script']}
   ExecStop=/bin/kill -15 $MAINPID
 
-  User=node['tomcat']['user']
-  Group=node['tomcat']['group']
+  User=#{node['tomcat']['user']}
+  Group=#{node['tomcat']['group']}
   UMask=0007
   RestartSec=10
   Restart=always
@@ -78,4 +70,3 @@ systemd_unit 'tomcat.service' do
   triggers_reload true
   action [ :create, :start, :enable ]
 end
-
